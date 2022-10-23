36f00a5cb47eee3e25e7bb2bea951f3cd8a9c72d
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-23 17:36:14 -0500

    Register component updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { register } from '../actions/actions'
-
-
+import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+// import Container from 'react-bootstrap/esm/Container';
 
 const Register = () => {
   const [fullname, setFullName] = useState()
@@ -26,76 +26,69 @@ const Register = () => {
 
   return (
     <>
-
-    <container>
-
-      <div className='justify-center ml-10 mr-10 '>
-        <div className='justify-center col-lg-7 py-3 py-md-5'>
-          <h1 className='text-center font-serif text-red-600 mt-10 text-bold'>
-            Register
-          </h1>
-          <br />
-          <p className='font-serif container mx-auto whitespace-normal flex-auto center-alig mr-20 text-lg'>
-            Once registered, you will be able to search for episodes of your
-            favorite podcast, save episodes to listen to in the future and keep
-            track of episodes you have already heard.  Lastly, you are able to hear the podcast from within the app itself.
-          </p>
-        </div>
-
-
-        
-        <div className='static font-serif  text-slate-500'>
-          <form onSubmit={handleSubmit}>
-            <div className='form-group'>
-              <input
-                onChange={e => setFullName(e.target.value)}
-                id='fullName-register'
-                name='fullName'
-                className='border-1 border-red-600 w-100 h-19 text-lg file:form-control'
-                type='text'
-                placeholder='Enter your Name'
-                autoComplete='off'
-              />
-            </div>
+      <container>
+        <div className='justify-center ml-10 mr-10 '>
+          <div className='  py-3 py-md-5'>
+            <h1 className=' font-sans text-red-600 mt-10 text-bold'>
+              Create an Account
+            </h1>
             <br />
-            <div className='form-group'>
-              {''}
-              <input
-                onChange={e => setEmail(e.target.value)}
-                id='email-register'
-                name='email'
-                className='border-1 border-red-600 w-100 h-19 text-lg file:form-control'
-                type='text'
-                placeholder='Enter your email '
-                autoComplete='off'
-              />
-            </div>
-            <br />
-            <div className='form-group'>
-       
-       
-              <input
-                onChange={e => setPassword(e.target.value)}
-                id='password-register'
-                name='password'
-                className='border-red-700 w-100 h-19 text-lg file:form-control'
-                type='password'
-                placeholder='Create a password'
-              />
-            </div>
-            <button
-              type='submit'
-              className='font-serif py-20 mt-4 btn btn-md btn-danger btn-block'
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-      </div>
+            <p className='font-serif container mx-auto whitespace-normal flex-auto center-alig mr-20 text-lg'>
+              Once registered, you will be able to search for episodes of your
+              favorite podcast, save episodes to listen to in the future and
+              keep track of episodes you have already heard. Lastly, you are
+              able to hear the podcast from within the app itself.
+            </p>
+          </div>
 
+          <div className='static font-sans text-slate-500'>
+            <form onSubmit={handleSubmit}>
+              <div className='form-group'>
+                <input
+                  onChange={e => setFullName(e.target.value)}
+                  id='fullName-register'
+                  name='fullName'
+                  className='border-1 rounded border-black w-100 h-19 text-lg file:form-control'
+                  type='text'
+                  placeholder='Enter your Name'
+                  autoComplete='off'
+                />
+              </div>
+              <br />
+              <div className='form-group'>
+                {''}
+                <input
+                  onChange={e => setEmail(e.target.value)}
+                  id='email-register'
+                  name='email'
+                  className='border-1 rounded border-black w-100 h-19 text-lg file:form-control'
+                  type='text'
+                  placeholder='Enter your email '
+                  autoComplete='off'
+                />
+              </div>
+              <br />
+              <div className='form-group'>
+                <input
+                  onChange={e => setPassword(e.target.value)}
+                  id='password-register'
+                  name='password'
+                  className='border-1 rounded border-black w-100 h-19 text-lg file:form-control'
+                  type='password'
+                  placeholder='Create a password'
+                />
+              </div>
+              <button
+                type='submit'
+                className='font-sans mt-3
+              ml-3 pl-3 py-2 px-4 rounded font-bold text-white bg-red-600'
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </container>
-
-
     </>
   )
 }
