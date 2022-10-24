e5bdfdc94e2d4466fe6224bbd068028e5d842334
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-24 10:51:13 -0500

    flash Message

---
+++ src/components/FlashMessage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from 'react-dom'
+import FlashMessage from 'react-flash-message'
+
+const FlashMessage = () => {
+  return (
+    <div>FlashMessage</div>
+  )
+}
+
+export default FlashMessage
